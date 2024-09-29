75fbf11630c3c8fea7f236a732a77e88968886e0
panzazeta <76927307+panzazeta@users.noreply.github.com>
2024-09-29 11:14:22 -0300

    add: full CRUD ok

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -40,3 +40,36 @@ export const postUser = async (req,res) => {
 
 }
 
+export const putUser = async (req,res) => {
+    const {id} = req.params;
+    const {nombre, apellido, edad, email} = req.body
+	
+    if (!nombre || !apellido || !edad || !email) {
+        return res.status(400).send({respuesta: 'Error', mensaje: 'Faltan datos obligatorios'});
+    }
+
+    try {
+        const user = await userModel.findByIdAndUpdate(id, {nombre, apellido, edad, email});
+        if (user)       
+            res.status(201).send({respuesta: 'OK', mensaje: user});
+        else 
+            res.status(404).send({respuesta: 'Error', mensaje: "User not found"})
+    } catch (error) {
+        res.status(400).send({respuesta: 'Error', mensaje: error.message});
+    }
+
+}
+
+export const deleteUser = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+         const userDeleted = await userModel.findByIdAndDelete(id);
+         if (!userDeleted) {
+            return res.status(404).send({ respuesta: 'Error', mensaje: 'User not found' });
+        }
+        res.status(200).send({ respuesta: 'OK', mensaje: 'Usuario eliminado' });
+    } catch (error) {
+            res.status(400).send({respuesta: 'Error', mensaje: error.message});
+    }
+}
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getUsers, getUserById, postUser } from "../controllers/users.controller.js";
+import { getUsers, getUserById, postUser, putUser, deleteUser } from "../controllers/users.controller.js";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUserById);
 userRouter.post('/', postUser);
+userRouter.put('/:id', putUser);
+userRouter.delete('/:id', deleteUser);
 
 export default userRouter;
 
